c496ffd317e29a0c81e4f477eb46fc9221cf6a00
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2025-12-14 12:07:03 +0530

    feat(Transaction-Request):Added Services.

---
+++ transaction_system/src/main/java/org/example/service/InMemoryTransactionService.java
@@ -0,0 +1,50 @@
+package org.example.service;
+
+import org.example.DTO.TransactionViewDTO;
+
+import org.example.model.Account;
+import org.example.model.Transaction;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class InMemoryTransactionService implements TransactionService {
+
+
+    @Override
+    public List<TransactionViewDTO> readAllTransactionsFromAccount(Account account) {
+        List<Transaction> transaction=account.getTransactions();
+
+        List<TransactionViewDTO> transactionViewDTO=new LinkedList<TransactionViewDTO>();
+        for(Transaction t:transaction){
+            transactionViewDTO.add(new TransactionViewDTO(
+                    t.getReceiver(),
+                    t.getTransactionId(),
+                    t.getSender(),
+                    t.getTransactionType(),
+                    t.getTransactionInitiationTime(),
+                    t.getStatus(),
+                    t.getPaymentService(),
+                    t.getAmount()
+            ));
+        }
+
+        return transactionViewDTO;
+    }
+
+    @Override
+    public Transaction createTransaction(Transaction transaction,Account account) {
+        account.addTransaction(transaction);
+        return transaction;
+    }
+
+    @Override
+    public Transaction readTransactionById(Long transactionId, Account account) {
+        return account.getTransactions().stream().
+                        filter(t ->
+                        t.getTransactionId().equals(transactionId))
+                        .findFirst().orElse(null);
+    }
+}
