b85ef736cc29ade90645a34b294f5ec9b7de312c
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2025-12-18 10:43:42 +0530

    fix(Transaction-Request):create TransactionHelper which contain common methods for different payment service.

--- transaction_system/src/main/java/org/example/business/MakeTransactionThroughMobileGateWay.java
+++ transaction_system/src/main/java/org/example/business/MakeTransactionThroughMobileGateWay.java
@@ -1,119 +1,31 @@
 package org.example.business;
 
-import org.example.exceptions.ThrowExcpetions;
 import org.example.model.*;
 import org.example.stats.PaymentStatsRouter;
 
-import java.time.LocalDateTime;
-
 public class MakeTransactionThroughMobileGateWay implements IMakeTransactions {
     private final PaymentStatsRouter statsRouter;
-
+    private final TransactionHelper transactionHelper;
     public MakeTransactionThroughMobileGateWay(PaymentStatsRouter statsRouter) {
         this.statsRouter = statsRouter;
+        this.transactionHelper=new TransactionHelper(statsRouter,PaymentService.MobileGateway);
     }
 
     //for deposit/withdraw into account
-    private Transaction getTransaction(Account sender,Account receiver,Double Amount){
-        Transaction transaction = new Transaction();
-        transaction.setTransactionInitiationTime(LocalDateTime.now());
-        transaction.setPaymentService(PaymentService.MobileGateway);
-        transaction.setTransactionId(Utility.generateUniqueLongId());
-        transaction.setStatus(TransactionStatus.INITIATION);
-        transaction.setTransactionType(TransactionType.TRANSFER);
-        transaction.setStatus(TransactionStatus.PENDING);
-        transaction.setSender(sender);
-        transaction.setReceiver(receiver);
-        transaction.setAmount(Amount);
-        return transaction;
-    }
+
 
     @Override
     public void SelfTransaction(Account account, Double Amount, boolean isDeposit) {
-        Double updatedAccountBalance,currentAccountBalance;
-        currentAccountBalance=updatedAccountBalance=account.getAccountBalance();
-        Transaction transaction =getTransaction(account,account,Amount);
-        if(isDeposit){
-            transaction.setTransactionType(TransactionType.CREDIT);
-            updatedAccountBalance=currentAccountBalance+Amount;
-            System.out.println("Current Account Balance is "+currentAccountBalance);
-            System.out.println("Updated Account Balance is "+updatedAccountBalance);
-        }
-        else{
-            transaction.setTransactionType(TransactionType.WITHDRAW);
-            try{
-                if(currentAccountBalance<Amount){
-
-                    throw new Exception("Insufficient Balance");
-                }
-                else{
-                    updatedAccountBalance=currentAccountBalance-Amount;
-
-                }
-            }
-            catch (Exception e){
-                System.out.println(e.getMessage());
-                account.addTransaction(transaction);
-                transaction.setStatus(TransactionStatus.FAILED);
-                statsRouter.submit(transaction);
-                return;
-            }
-        }
-        account.setAccountBalance(updatedAccountBalance);
-        account.addTransaction(transaction);
-        transaction.setStatus(TransactionStatus.COMPLETED);
-        statsRouter.submit(transaction);
+        transactionHelper.doSelfTransaction(account,Amount,isDeposit);
     }
-
     @Override
     public void transferTransaction(Account sender, Account receiver, Double Amount,boolean willCancel)  {
-
-        Transaction transaction=getTransaction(sender,receiver,Amount);
-        //* critical section starts here
-        transaction.setTransactionType(TransactionType.TRANSFER);
-        transaction.setAmount(Amount);
-        Double currentAccountBalanceSender=sender.getAccountBalance();
-        Double currentAccountBalanceReceiver=receiver.getAccountBalance();
-        try{
-            if(willCancel){
-                ThrowExcpetions.general("Request has to cancel, failed transactio");
-            }
-            if(currentAccountBalanceSender<Amount){
-
-                throw new Exception("Insufficient Balance");
-            }
-            else{
-
-                currentAccountBalanceSender=currentAccountBalanceSender-Amount;
-                sender.setAccountBalance(currentAccountBalanceSender);
-                currentAccountBalanceReceiver=currentAccountBalanceReceiver+Amount;
-                receiver.setAccountBalance(currentAccountBalanceReceiver);
-            }
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-            //if there is insufficient balance means sender has failed transaction
-            sender.addTransaction(transaction);
-            receiver.addTransaction(transaction);
-            transaction.setStatus(TransactionStatus.FAILED);
-            statsRouter.submit(transaction);
-            return;
-        }
-        //critical section ends here
-        sender.addTransaction(transaction);
-        receiver.addTransaction(transaction);
-        transaction.setStatus(TransactionStatus.COMPLETED);
-        statsRouter.submit(transaction);
+        transactionHelper.doTransferTransaction(sender,receiver,Amount,willCancel);
     }
 
     @Override
     public RequestTransaction RequestTransaction(Account sender, Account receiver, Double Amount) {
-        Transaction transaction=getTransaction(sender, receiver, Amount);
-        RequestTransaction requestTransaction = new RequestTransaction();
-        requestTransaction.setTransaction(transaction);
-        requestTransaction.setRequestTransactionId(Utility.generateUniqueLongId());
-        requestTransaction.setRequestStatus(RequestStatus.PENDING);
-        return requestTransaction;
+        return transactionHelper.doRequestTransaction(sender,receiver,Amount);
     }
 
 
--- transaction_system/src/main/java/org/example/business/Utility.java
+++ transaction_system/src/main/java/org/example/business/Utility.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 public class Utility {
     public static long generateUniqueLongId() {
         UUID uuid = UUID.randomUUID();
+
+
         long uniqueLong = uuid.getMostSignificantBits() & Long.MAX_VALUE;
         return uniqueLong;
     }
